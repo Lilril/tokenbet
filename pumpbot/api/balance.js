93730813f9321f10da4822788dca5241b09faaa6
Lilril <52168171+Lilril@users.noreply.github.com>
2026-01-26 15:51:37 +0200

    Update balance.js

--- pumpbot/api/balance.js
+++ pumpbot/api/balance.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
   const walletAddress = req.query.wallet;
-  const tokenMint = req.query.token || '7MDApufGNAKDDTcM3hrUpUfZ8jcaXdaoSZy1sQdRpump';
+  const tokenMint = req.query.token || '2KhMg3yGW4giMYAnvT28mXr4LEGeBvj8x8FKP5Tfpump';
   
   if (!walletAddress) {
     return res.status(400).json({
