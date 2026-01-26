b16114621439e7748d0993fc9a23dd3815e49bf8
Lilril <52168171+Lilril@users.noreply.github.com>
2026-01-26 15:39:13 +0200

    Update app.js

--- pumpbot/js/app.js
+++ pumpbot/js/app.js
@@ -1,7 +1,7 @@
 // ============================================
 // КОНФИГУРАЦИЯ
 // ============================================
-const TOKEN_ADDRESS = '2KhMg3yGW4giMYAnvT28mXr4LEGeBvj8x8FKP5Tfpump';
+const TOKEN_ADDRESS = '7MDApufGNAKDDTcM3hrUpUfZ8jcaXdaoSZy1sQdRpump';
 
 let wallet = null;
 let selectedInterval = 15;
@@ -405,3 +405,4 @@ window.addEventListener('load', async () => {
         }
     }, 10000);
 });
+
